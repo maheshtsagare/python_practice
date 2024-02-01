a8382f190e1ae290dfe137ed2296b7f177fbdb2c
maheshtsagare <113876049+maheshtsagare@users.noreply.github.com>
2024-02-01 07:21:15 +0000

    Basics_of_python

--- basics/hello.py
+++ basics/hello.py
@@ -1,2 +1,20 @@
-print("hello world")
-print(2+3)
+print("hello world","mY NAME IS mAHESH")
+print(25+35)
+
+# variabales
+
+name="mahesh"
+age=26
+price=25.99
+
+print("my name is",name,"_","my  age is",age,"_","price is",price)
+
+# Datatypes
+print(type(name))
+print(type(age))
+print(type(price))
+
+old=False
+a=None
+print(type(a))
+print(type(old))
